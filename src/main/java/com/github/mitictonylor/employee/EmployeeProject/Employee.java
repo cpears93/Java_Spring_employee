cc43ecf3f4dd9bf0312481c8f28c67fd5090bd9a
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-09 11:35:28 +0100

    add Employee class, constructor, default constructors, getters and setters, plus set up for DB

---
+++ src/main/java/com/github/mitictonylor/employee/EmployeeProject/Employee.java
@@ -0,0 +1,62 @@
+package com.github.mitictonylor.employee.EmployeeProject;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "first_name")
+    private String firstName;
+
+    @Column(name= "last_name")
+    private String lastName;
+
+    @Column(name= "employee_number")
+    private int employeeNumber;
+
+    public Employee(String firstName, String lastName, int employeeNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+}

06cc3b36cc85f2bb303d325bf0d7575c1cb6422e
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-09 11:54:24 +0100

    creates Project class, constructor, getters and setters

---
+++ src/main/java/com/github/mitictonylor/employee/EmployeeProject/modules/Project.java
@@ -0,0 +1,63 @@
+package com.github.mitictonylor.employee.EmployeeProject.modules;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int duration;
+
+    private List<Employee> employeeList;
+
+    public Project(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+        this.employeeList = new ArrayList<Employee>();
+    }
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+}
